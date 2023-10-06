097b66c588923990a2b40373eab5c5c39f331877
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-10-06 13:42:25 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Funguju')
+console.log('Funguju');
+
+const buttons = document.querySelectorAll('.kamen');
+
+const changeTitle = (e) => {
+  const zvuk = new Audio(`tony/${e.target.textContent}.mp3`);
+  zvuk.play();
+
+  const title = document.querySelector('h1');
+  title.textContent = e.target.textContent;
+};
+
+buttons.forEach((button) => {
+  button.addEventListener('click', changeTitle);
+});
